73d0079d1106fd05aefc9f485bb77da7abd8e818
boitan <yongrou74@hotmail.com>
2021-05-06 00:01:35 +0800

    save

--- home/models.py
+++ home/models.py
@@ -6,12 +6,20 @@ from django.urls import reverse
 
 class Home(models.Model):
     title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255,default='')
+    button_text = models.CharField(max_length=255,default='')
     # subtitle = models.CharField()
-    content = models.TextField()
-    content_two = models.TextField()
-    list_content = models.TextField()
-    detail_word = models.TextField()
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    about_title = models.TextField(default='')
+    about_description = models.TextField(default='')
+
+    service_title = models.TextField(default='')
+    # content = models.TextField()
+    # content_two = models.TextField()
+    list_content = models.TextField(default='')
+    detail_word = models.TextField(default='')
+    imageone = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    imagetwo = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    imagethree = models.ImageField(default='default.jpg', upload_to='profile_pics')
     date_posted = models.DateTimeField(default=timezone.now)
     # author = models.ForeignKey(User, on_delete=models.CASCADE)
     
